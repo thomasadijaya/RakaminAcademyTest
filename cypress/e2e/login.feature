a0fc83517c0b24a8e2d7c023b926725deab8bd8a
Thomas Wijaya <thomasadijaya77@gmail.com>
2023-04-05 11:28:30 +0700

    Generate html report

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -1,8 +1,13 @@
 Feature: Login 
 
-    Scenario: User are able to login using valid credential
+    Background:
         Given User are in the login page
-        When User enter the valid credential
+
+    Scenario: User are able to login using valid credential
+        When User enter the valid email 'kutukucing16@gmail.com' and password 'kutukucing123' 
         Then User are successfully login
 
-    # Scenario: User cannot login using invalid password
+    Scenario: User cannot login using invalid password
+        When User enter the valid email 'kutukucing16@gmail.com'
+        But User enter the invalid password 'kutukucing1234567'
+        Then User are failed to login
--- cypress/e2e/login/login.js
+++ cypress/e2e/login/login.js
@@ -1,15 +1,28 @@
-import {Given,When,And,Then} from "cypress-cucumber-preprocessor/steps";
+import {Given,When,And,But,Then} from "cypress-cucumber-preprocessor/steps";
 
 Given("User are in the login page", function(){
     cy.visit('https://web-staging.rakamin.com/login')
 })
 
-When("User enter the valid credential", function(){
-    cy.get('[data-cy="login-email-text-field"]').type('kutukucing16@gmail.com')
-    cy.get('[data-cy="login-password-text-field"]').type('kutukucing123')
+When("User enter the valid email {string} and password {string}", function(email,password){
+    cy.get('[data-cy="login-email-text-field"]').type(email)
+    cy.get('[data-cy="login-password-text-field"]').type(password)
+    cy.get('[data-cy="login-submit-button"]').click()
+})
+
+When("User enter the valid email {string}", function(email){
+    cy.get('[data-cy="login-email-text-field"]').type(email)
+})
+
+But("User enter the invalid password {string}", function(password){
+    cy.get('[data-cy="login-password-text-field"]').type(password)
     cy.get('[data-cy="login-submit-button"]').click()
 })
 
 Then("User are successfully login", function(){
     cy.get('[data-cy="dashboard-header-title"]').should('have.text','Courses')
+})
+
+Then("User are failed to login", function(){
+    cy.get('.sc-ilEZps').should('have.text','Kata sandi anda salah')
 })
